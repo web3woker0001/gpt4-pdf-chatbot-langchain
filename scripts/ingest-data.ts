4f2479bed702fdc9436bcec6308b72b2fe3f6e13
harry <machengxue707@gmail.com>
2023-11-01 18:49:03 +0800

    fix pincode issue

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -2,7 +2,7 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { pinecone } from '@/utils/pinecone-client';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 
@@ -15,7 +15,10 @@ export const run = async () => {
   try {
     /*load raw docs from the all files in the directory */
     const directoryLoader = new DirectoryLoader(filePath, {
-      '.pdf': (path) => new PDFLoader(path),
+      '.txt': (path) => new TextLoader(path),
+//       const loader = new TextLoader("src/document_loaders/example_data/example.txt");
+// const docs = await loader.load();
+//       '.pdf': (path) => new PDFLoader(path),
     });
 
     // const loader = new PDFLoader(filePath);
@@ -38,7 +41,7 @@ export const run = async () => {
     //embed the PDF documents
     await PineconeStore.fromDocuments(docs, embeddings, {
       pineconeIndex: index,
-      namespace: PINECONE_NAME_SPACE,
+      // namespace: PINECONE_NAME_SPACE,
       textKey: 'text',
     });
   } catch (error) {
